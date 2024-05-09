8d4f53406d809e6d35dd277bcbad6d95fd40fe6f
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-05-09 17:32:18 +0100

    expense tracker

---
+++ exptracker/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .forms import ExpenseForm
+
+# Create your views here.
+
+
+def index(request):
+    expense_form = ExpenseForm()
+    return render(request, 'myapp/index.html', {'expense_form': expense_form})
